d08d003f532ec0c92901532cdace3ccaccb2b917
Tradeobrih <romera.valladolid@gmail.com>
2024-10-09 12:55:49 +0000

    Cuarto Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const resultadoElemento = document.getElementById("resultado");
+const botonGenerar = document.getElementById("boton-generar");
+
+const actualizarResultado = () => {
+  resultadoElemento.innerHTML = generarDominio();
+};
+
+window.onload = () => {
+  actualizarResultado();
+};
+
+botonGenerar.addEventListener("click", actualizarResultado);
+
+const frases = [
+  ["the", "our"],
+  ["great", "big"],
+  ["jogger", "racoon"],
+  [".com", ".es", ".net"]
+];
+
+const obtenerIndiceAleatorio = arr => Math.floor(Math.random() * arr.length);
+
+const generarDominio = () => {
+  let partesDominio = [];
+  for (let i = 0; i < frases.length; i++) {
+    partesDominio.push(frases[i][obtenerIndiceAleatorio(frases[i])]);
+  }
+  return partesDominio.join("");
 };
